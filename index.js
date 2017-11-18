4f947cc528e2e347d116a329cf9c726c9abd5745
Narek Khachatryan <narek1993x@mail.ru>
2017-11-18 15:13:51 +0400

    add cors for localhost, and config.json for mongoose connect

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const config = require('./config/config.json');
 
 // DB Setup
-mongoose.connect('mongodb://localhost:auth/auth');
+mongoose.connect(`mongodb://${config.db.host}:${config.db.port}/${config.db.dbName}`, {
+  useMongoClient: true,
+})
 
 // App Setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json());
 router(app);
 
